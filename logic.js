9e46d0390b172625ff494acdab30d1c2a8d93b4b
jrtrent <jessrtrent@gmail.com>
2018-03-29 00:03:10 -0400

    hw

--- logic.js
+++ logic.js
@@ -1,75 +1,69 @@
 var reactions = ["eyeroll", "facepalm", "happy", "high five", "kiss", "lol", "no", "wink"];
 
 $(document).ready(function renderbuttons() {
-     $("#addreaction").empty();
+    $("#addreaction").empty();
     for (var i = 0; i < reactions.length; i++) {
-        var reactionbutton = $("<button>");
-        reactionbutton.addClass("#reactions");
-        reactionbutton.addClass("btn btn-primary")
-		reactionbutton.attr("title", reactions[i]);
-        reactionbutton.text(reactions[i]);
+        var reactionbutton = $("<button>").text(reactions[i]);
+        reactionbutton.addClass("reactions");
+        reactionbutton.addClass("btn btn-primary");
+        reactionbutton.attr("value", reactions[i]);
         $("#reactionsgif").append(reactionbutton);
-		console.log("title");
-
+    
+                
+    }
         
-
-    };
-     
 });
+//$("#addreaction").on("click", function (event) {
+   // event.preventDefault();
+    //var newreaction = $("#reaction-input").val().trim();
+    //reactions.push(newreaction);
+    //$("reaction-input").val('')
+    //console.log(reactions);
+
+//});
+      
 
 
+$(document).on("click", ".reactions",function (){
+    $("#gifresults").empty();
+   var title = $(this).val();
+    console.log(title);
 
-    $("#addreaction").on("click", function(event) {
-        event.preventDefault();
-        var newreaction = $("#reaction-input").val().trim();
-        reactions.push(newreaction);
-        $("reaction-input").val('')
-        console.log(reactions);
-        
-    });
-		
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DbTrA1zDtXF3YA0imZIJ1GeDSwTgnsrv&q=" + title + "&limit=25&offset=0&lang=en";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+    .then(function(response){
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++){
+            var moving = response.data[i].fixed_height_small.url;
+            var still = response.data[i].images.original_still.url;
+            var gifimage = $("<img>").attr("src", moving);
+            var gifstill = $("<img>").attr("src", still);
+            var rating = $("<p>").text( "Rating: " + response.data[i].rating);
+            rating.addClass("gif");
+            $("#gifresults").append(rating);
+            $("#gifresults").append(gifimage);                           
+         
+        } 
 
-    $(".reactions").on("click",function () {
-		var title = $(this).data("title");
-		console.log("title");
-        
-       // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DbTrA1zDtXF3YA0imZIJ1GeDSwTgnsrv&q=" + title + "&limit=25&offset=0&lang=en";
-       
-       // $.ajax({
-           // url: queryURL,
-           // method: "GET"
-      //  })
-        //.then(function(response){
-           // console.log(response);
-            //var imageURL = response.data.embed_url;
-            //var gifimage =$("<img>");
-            //gifimage.attr("src",imageURL);
-           // gifimage.attr("alt","reactiongif");
-          //  ("#reactionsgif").prepend(gifimage); 
-        //})
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
     })
 
    
     
+    
+      
 
+})
 
-
-
-
- 
-
-    //
-  //  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DbTrA1zDtXF3YA0imZIJ1GeDSwTgnsrv&q=eyeroll&limit=25&offset=0&rating=G&lang=en";
-
-      // $.ajax({
-         //   url: queryURL,
-          // method:"GET"
-  //  })
-
-   // .then(function(response){
-    //   console.log(url);
-
-
-   // });
-//})
 
